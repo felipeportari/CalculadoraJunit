8b45c35f86333a476469a810eadfad3d97301ce2
felipeportari <felipe.portari7@gmail.com>
2023-09-21 19:35:58 -0300

    Aprimorando o código - Resultados mais limpos

--- src/main/java/com/felipe/Calculadora.java
+++ src/main/java/com/felipe/Calculadora.java
@@ -4,21 +4,29 @@ public class Calculadora {
 
     // calcula a soma de dois parâmetros
     public int soma(int a, int b) {
+        System.out.println("");
+        System.out.println("O resultado da soma entre " + a + " e " + b + " é:");
         return a + b;
     }
 
     // calcula a subtração de dois parâmetros
     public int subtracao(int a, int b) {
+        System.out.println("");
+        System.out.println("O resultado da subtração entre " + a + " e " + b + " é:");
         return a - b;
     }
 
     // calcula a divisão de dois parâmetros
     public int divisao(int a, int b) {
+        System.out.println("");
+        System.out.println("O resultado da divisão entre " + a + " e " + b + " é:");
         return a / b;
     }
 
     // calcula a multiplicação de dois parâmetros
     public int multiplicacao(int a, int b) {
+        System.out.println("");
+        System.out.println("O resultado da multiplicação entre " + a + " e " + b + " é:");
         return a * b;
     }
 }
