6b2df84193b6f97e109a268046a0ae5b62e6532f
felipeportari <felipe.portari7@gmail.com>
2023-09-21 00:24:25 -0300

    Calculadora comum concluída (projeto feito em maven)

---
+++ src/main/java/com/felipe/Calculadora.java
@@ -0,0 +1,24 @@
+package com.felipe;
+
+public class Calculadora {
+
+    // calcula a soma de dois parâmetros
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    // calcula a subtração de dois parâmetros
+    public int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    // calcula a divisão de dois parâmetros
+    public int divisao(int a, int b) {
+        return a / b;
+    }
+
+    // calcula a multiplicação de dois parâmetros
+    public int multiplicacao(int a, int b) {
+        return a * b;
+    }
+}
